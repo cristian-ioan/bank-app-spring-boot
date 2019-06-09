5bc724f61313965ae08054edd27f26e09eb57275
Cristian Lucian IOAN <cristian-ioan@users.noreply.github.com>
2019-06-09 16:28:36 +0300

    username/password

--- src/main/java/com/banking/util/ScheduleUtils.java
+++ src/main/java/com/banking/util/ScheduleUtils.java
@@ -4,8 +4,8 @@ public final class ScheduleUtils {
 
     public static final int NUMBER_OF_MINUTES_JOB_EXPIRED = 5;
     public static final int NUMBER_OF_SECONDS_FOR_DELETE_TOKEN = 90000;
-    public static final String USERNAME = "java2Iasi@gmail.com";
-    public static final String PASSWORD = "JavaIasi2018";
+    public static final String USERNAME = "yourEmail";
+    public static final String PASSWORD = "yourPassword";
 
     private ScheduleUtils(){}
 }
--- src/main/resources/populatePerson.sql
+++ src/main/resources/populatePerson.sql
@@ -1,4 +1,4 @@
-INSERT INTO `bank-app`.`person` (`id`, `address`, `email`, `first_name`, `last_name`, `user_id`) VALUES ('1', 'Iasi', 'ioan.cristian74@gmail.com', 'Bogdan', 'Bogdanel', '1');
-INSERT INTO `bank-app`.`person` (`id`, `address`, `email`, `first_name`, `last_name`, `user_id`) VALUES ('2', 'Waslui', 'ioan.cristian74@gmail.com', 'Iosif', 'Iosif', '2');
-INSERT INTO `bank-app`.`person` (`id`, `address`, `email`, `first_name`, `last_name`, `user_id`) VALUES ('3', 'Timisoara', 'ioan.cristian74@gmail.com', 'Ioan', 'Ionica', '3');
-INSERT INTO `bank-app`.`person` (`id`, `address`, `email`, `first_name`, `last_name`, `user_id`) VALUES ('4', 'Iasi', 'ioan.cristian74@gmail.com', 'Alex', 'Alexandru', '4');
+INSERT INTO `bank-app`.`person` (`id`, `address`, `email`, `first_name`, `last_name`, `user_id`) VALUES ('1', 'Iasi', 'abc@gmail.com', 'Bogdan', 'Bogdanel', '1');
+INSERT INTO `bank-app`.`person` (`id`, `address`, `email`, `first_name`, `last_name`, `user_id`) VALUES ('2', 'Waslui', 'bcd@gmail.com', 'Iosif', 'Iosif', '2');
+INSERT INTO `bank-app`.`person` (`id`, `address`, `email`, `first_name`, `last_name`, `user_id`) VALUES ('3', 'Timisoara', 'cde@gmail.com', 'Ioan', 'Ionica', '3');
+INSERT INTO `bank-app`.`person` (`id`, `address`, `email`, `first_name`, `last_name`, `user_id`) VALUES ('4', 'Iasi', 'def@gmail.com', 'Alex', 'Alexandru', '4');
